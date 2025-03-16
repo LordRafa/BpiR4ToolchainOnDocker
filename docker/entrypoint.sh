ea769921febaed361817e9a8c2b6cea984839333
Rafael Waldo Delgado Doblas <rawadeldo@protonmail.com>
2025-03-16 23:45:21 +0400

    Add necesary changes to download openwrt and start the toolchain container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 args="$@"
 
+if [ ! -d 'mtk-openwrt-feeds' ]; then
+    git clone https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds
+fi
+
+if [ ! -d 'openwrt' ]; then
+    git clone https://git.openwrt.org/openwrt/openwrt.git
+fi
+
+alias mtk-autobuild=~/mtk-openwrt-feeds/autobuild/unified/autobuild.sh
+
+cd openwrt
+
 exec $args
 
