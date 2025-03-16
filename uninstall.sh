9b6fbf8b7f0559285bbdc6ace6f235a9d7635b34
Rafael Waldo Delgado Doblas <rawadeldo@protonmail.com>
2025-03-16 23:54:13 +0400

    remove uneeded delete from uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,6 @@
 rm -r "$HOME/bin/bpi-r4-toolchain-onDocker.sh"
 rm -r "$HOME/.local/share/icons/hicolor/scalable/apps/bpi-r4-toolchain-onDocker.svg"
 rm -r "$HOME/.local/share/applications/bpi-r4-toolchain-onDocker.desktop"
-rm -r "$HOME/.onDocker/bpi-r4-toolchain"
 
 docker image rm lordrafa/bpi-r4-toolchain
 
